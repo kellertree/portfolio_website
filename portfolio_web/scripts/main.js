9410515251fa4120b4dad82c72ac87126b21b74d
Keller Tree <Katree@madisoncollege.edu>
2024-05-22 12:22:13 -0500

    Add files via upload

---
+++ portfolio_web/scripts/main.js
@@ -0,0 +1,14 @@
+// main.js
+
+$(document).ready(function() {
+    // Smooth scrolling on navigation link clicks
+    $('a[href^="#"]').on('click', function(event) {
+        var target = $(this.getAttribute('href'));
+        if (target.length) {
+            event.preventDefault();
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top
+            }, 1000);
+        }
+    });
+});
